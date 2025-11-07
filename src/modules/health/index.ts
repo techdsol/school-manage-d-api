3e69dcbcac21612657185537b736ff0a57dcbe8a
Rajesh Kappalvadi Pragasam <rkp@mac.lan>
2025-11-07 16:00:30 -0700

    refactor: add index.ts to all modules for consistent imports

---
+++ src/modules/health/index.ts
@@ -0,0 +1,8 @@
+// Services
+export { HealthService } from './services/health.service';
+
+// Controllers
+export { HealthController } from './controllers/health.controller';
+
+// Module
+export { HealthModule } from './health.module';
